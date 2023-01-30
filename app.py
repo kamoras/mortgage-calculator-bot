5abd5d6443014a93cf60c6d23337e6cb83807231
Ryan Mack <mack.ryanm@gmail.com>
2023-01-30 02:33:11 +0000

    Add thread start and enhance mortgage bot to be finance bot

--- app.py
+++ app.py
@@ -17,24 +17,50 @@ async def on_message(message):
     # If the author is the bot do nothing.
     if message.author == client.user:
         return
+    try:
+        if message.content.startswith("!financebot help"):
+            msg = "Welcome to finance bot!\n!financebot mortgage help\n!financebot mortgage <price> <down> <length> <interest>\n!financebot bondask help\nfinancebot bondask <coupon> <interest> <numPeriods> <bondValue>\n"
+            await message.channel.send(msg)
+        elif message.content.startswith("!financebot mortgage help"):
+            msg = "Calculate a mortgage payment by using the following command\n!mortgage <price> <down> <length> <interest>"
+            await message.channel.send(msg)
+        # of form !mortgage <price> <down> <length> <interest>
+        elif message.content.startswith("!financebot mortgage"):
+            split = message.content.split()
 
-    if message.content.startswith("!mortgage help"):
-        msg = "Calculate a mortgage payment by using the following command\n!mortgage <price> <down> <length> <interest>"
+            price = float(split[2])
+            down = float(split[3])
+            length = float(split[4]) * 12
+            interest = (float(split[5]) / 12) / 100
+
+            monthly_payment = (price - down) * interest * ((math.pow((1+interest), length))/((math.pow((1+interest), length))-1))
+
+            await message.channel.send("Monthly payment: $" + str(round(monthly_payment, 2)))
+        elif message.content.startswith("!financebot bondask help"):
+            msg = "Calculate bond ask by using the following command\n!bondask <coupon> <interest> <numPeriods> <bondValue>"
+            await message.channel.send(msg)
+        elif message.content.startswith("!financebot bondask"):
+            split = message.content.split()
+            # ((coupon / 2) / (interest rate / 2)) * 1 - (1 / ((1 + (interest / 2))^(numPeriods-1)))
+
+            coupon = float(split[2])
+            interest = float(split[3]) / 100
+            num_periods = float(split[4])
+            bond_value = float(split[5])
+
+            p1 = float((coupon / 2) / (interest / 2))
+            p2 = (1 / math.pow(1 + float(interest / 2), num_periods - 1))
+            p3 = bond_value + (coupon / 2)
+            p4 = math.pow((1 + (interest / 2)), num_periods)
+
+            bond_ask = (p1 * (1 - p2)) + (p3 / p4)
+            await message.channel.send("Bond ask: $" + str(round(bond_ask, 2)))
+        elif message.content == "!financebot":
+            msg = "Incomplete financebot command. Valid commands are:\n!financebot mortgage help\n!financebot mortgage <price> <down> <length> <interest>\n!financebot bondask help\nfinancebot bondask <coupon> <interest> <numPeriods> <bondValue>\n"
+            await message.channel.send(msg)
+    except:
+        msg = "Invalid financebot command. Valid commands are:\n!financebot mortgage help\n!financebot mortgage <price> <down> <length> <interest>\n!financebot bondask help\nfinancebot bondask <coupon> <interest> <numPeriods> <bondValue>\n"
         await message.channel.send(msg)
-    # of form !mortgage <price> <down> <length> <interest>
-    elif message.content.startswith("!mortgage"):
-        split = message.content.split()
-
-        price = float(split[1])
-        down = float(split[2])
-        length = float(split[3]) * 12
-        interest = (float(split[4]) / 12) / 100
-
-        monthly_payment = (price - down) * interest * ((math.pow((1+interest), length))/((math.pow((1+interest), length))-1))
-
-        await message.channel.send("Monthly payment: $" + str(round(monthly_payment, 2)))
-    
-
 
 @client.event
 async def on_ready():
